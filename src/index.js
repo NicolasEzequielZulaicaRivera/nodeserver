e3c90cb7f15a1ad6cebde0342a8ac5f4ae54a97d
nicolas_zulaica <nzulaica@fi.uba.ar>
2022-04-06 15:02:06 -0300

    util routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import service_routes from "./routes/service.js";
+import util_routes from "./routes/util.js";
 dotenv.config();
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/service", service_routes);
+app.use("/api", util_routes);
 
 // Server
 const server = app.listen(port, () => {
